d4e2b889ada7d1ae5a55b938ea2812ed3b4e8441
vicky181294 <vcenavicky@gmail.com>
2025-04-08 18:33:15 +0530

    modified deplo#7

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -53,6 +53,13 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
