08c9b413ef444c2ec93f6f7b3f5b2c7339c8ea4f
vicky181294 <vcenavicky@gmail.com>
2025-04-08 18:41:07 +0530

    modified deplo#8

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -82,21 +82,21 @@ resource "aws_lb" "app_lb" {
   security_groups    = [aws_security_group.ecs_sg.id]
 }
 
-resource "aws_lb_target_group" "patient" {
-  name        = "tg-patient"
-  port        = var.container_port
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-}
+# resource "aws_lb_target_group" "patient" {
+#   name        = "tg-patient"
+#   port        = var.container_port
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.main.id
+#   target_type = "ip"
+# }
 
-resource "aws_lb_target_group" "appointment" {
-  name        = "tg-appointment"
-  port        = var.container_port
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-}
+# resource "aws_lb_target_group" "appointment" {
+#   name        = "tg-appointment"
+#   port        = var.container_port
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.main.id
+#   target_type = "ip"
+# }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_lb.arn
